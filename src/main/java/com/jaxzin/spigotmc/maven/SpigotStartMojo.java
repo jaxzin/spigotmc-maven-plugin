43893817f60ac98996d8cd8986b9ab04e2a87b14
Brian R. Jackson <brian@jaxzin.com>
2016-01-16 23:52:26 -0800

    First version of SpigotStartMojo that starts...but hangs.

--- src/main/java/com/jaxzin/spigotmc/maven/SpigotStartMojo.java
+++ src/main/java/com/jaxzin/spigotmc/maven/SpigotStartMojo.java
@@ -7,6 +7,13 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.bukkit.craftbukkit.Main;
+import org.bukkit.craftbukkit.libs.joptsimple.OptionException;
+import org.bukkit.craftbukkit.libs.joptsimple.OptionParser;
+import org.bukkit.craftbukkit.libs.joptsimple.OptionSet;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 /**
  *  <p>
@@ -21,13 +28,26 @@ import org.bukkit.craftbukkit.Main;
  *
  */
 @Mojo(
-        name = "start",
-        requiresDependencyResolution = ResolutionScope.TEST,
-        executionStrategy = "phase='validate'"
+    name = "start",
+    requiresDependencyResolution = ResolutionScope.TEST,
+    executionStrategy = "phase='validate'"
 )
 public class SpigotStartMojo extends AbstractMojo {
-        public void execute() throws MojoExecutionException, MojoFailureException {
-                getLog().info("Starting Spigot...");
-                Main.main(new String[]{});
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Starting Spigot...");
+        try {
+//            Main.main(new String[]{});
+            // TODO: Get the spigot jar in a less fragile way (ex. copy into local dir with dependency plugin)
+            String[] args = {"/bin/bash", "-c", "java -jar ~/.m2/repository/org/spigotmc/spigot/1.8.8/spigot-1.8.8.jar"};
+            Process process =
+                new ProcessBuilder(args)
+                    .redirectErrorStream(true)
+                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .start();
+            process.waitFor();
+        } catch (Throwable t) {
+            throw new MojoFailureException("Unable to start Spigot server.", t);
         }
+    }
+
 }
